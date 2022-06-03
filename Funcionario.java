f4363e985d914d3b63d6a2915c0f3da433bdf327
Lorena-Lima <lorenalima.eds@gmail.com>
2022-06-03 14:05:56 -0300

    Acrescentando as funções pulaLinha(), escrevaDesconto(), EscrevaBonus() e melhorando o IF.

--- Funcionario.java
+++ Funcionario.java
@@ -3,6 +3,8 @@ public class Funcionario {
     double salario;
     double bonus;
     double salarioLiquido;
+    double desconto;
+
     public void setName(String name) {
         this.name = name;
     }
@@ -18,15 +20,37 @@ public class Funcionario {
     public void setSalario(Double salario) {
         this.salario = salario;
     }
+    public void escrevaBonus(){
+        System.out.println("Salário: " + salario);
+        System.out.println("Bônus: " + bonus);
+        System.out.println("Salário líquido: " + salarioLiquido);
+    }
+    public void escrevaDesconto(){
+        System.out.println("Salário: " + salario);
+        System.out.println("Desconto: " + desconto);
+        System.out.println("Salário líquido: " + salarioLiquido);
+    }
+    public void pulaLinha(){
+        System.out.println();
+        System.out.println();
+    }
     public void calculaSalario(double salario){
         if (salario <= 1000.00){
             this.salario = salario;
             bonus = salario * 0.2;
             salarioLiquido = salario * 1.2;
+            escrevaBonus();
+        }if(salario>1000 && salario<=2000){
+            this.salario = salario;
+            bonus = salario * 0.1;
+            salarioLiquido = salario * 1.1;
+            escrevaBonus();
+        }if(salario>2000){
+            this.salario = salario;
+            desconto = salario * 0.1;
+            salarioLiquido = salario * 0.9;
+            escrevaDesconto();
         }
-        System.out.println("Salário: " + salario);
-        System.out.println("Bônus: " + bonus);
-        System.out.println("Salário líquido: " + salarioLiquido);
     }
 
 
