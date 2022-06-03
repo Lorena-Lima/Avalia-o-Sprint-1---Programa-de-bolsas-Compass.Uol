d0fd6486121b995d4b5621654e832f6f8ade4b91
Lorena-Lima <lorenalima.eds@gmail.com>
2022-06-03 14:06:45 -0300

    acrescentado dois novos funcionários

--- bonificacaoSalarial.java
+++ bonificacaoSalarial.java
@@ -1,21 +1,26 @@
 public class bonificacaoSalarial {
     public static void main(String[] args) {
-        Funcionario primeiro = new Funcionario();
-        primeiro.setName("Lorena Lima Pereira");
-        primeiro.setSalario(1000.00);
-        System.out.println(primeiro.getName());
-        primeiro.calculaSalario(1000);
+        Funcionario lorena = new Funcionario();
+        lorena.setName("Lorena Lima Pereira");
+        lorena.setSalario(1200.00);
+        System.out.println(lorena.getName());
+        lorena.calculaSalario(1200);
+        lorena.pulaLinha();
 
+        Funcionario marcio = new Funcionario();
+        marcio.setName("Marcio Rezende Pereira");
+        marcio.setSalario(1000.00);
+        System.out.println(marcio.getName());
+        marcio.calculaSalario(1000);
+        marcio.pulaLinha();
 
+        Funcionario ana = new Funcionario();
+        ana.setName("Ana Lima Pereira");
+        ana.setSalario(2500.00);
+        System.out.println(ana.getName());
+        ana.calculaSalario(2500.00);
+        ana.pulaLinha();
 
-        /*if(primeiro.salario<= 1000.00 ){
-            primeiro.salario = primeiro.salario * 1.2;
-            primeiro.bonus = primeiro.salario *
-        }if( primeiro.salario< 2000.00 && primeiro.salario > 1000.00){
-            primeiro.salario = primeiro.salario * 1.1;
-        }if(primeiro.salario > 2000.00){
-            primeiro.salario = primeiro.salario * 0.9;
-        }*/
 
 
 
